f66c01b4c8819c307210b4cd26485eb6bcd87ec4
Sarker Sunzid Mahmud <sunzid02@gmail.com>
2025-05-02 18:54:38 +0200

    api development starts

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 //DB Config
@@ -14,6 +18,12 @@ mongoose.connect(db)
 app.get('/', (req, res) => {
     res.send('Hello world!')   
 });
+
+
+//Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
