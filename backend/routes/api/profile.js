7d5f32bf8e363124a656d7595c3e2a35dc735e8f
Sarker Sunzid Mahmud <sunzid02@gmail.com>
2025-05-04 17:15:16 +0200

    - Current user profile route

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+// const keys = 
+
+//load profile model
+const Profile = require('../../models/Profile');
+
+//load User model
+const User = require('../../models/User');
 
 
 // @route  GET api/profile/test
@@ -10,5 +19,24 @@ router.get('/test', (req, res) => {
         msg: "Profile Works"
     })
 });
+
+
+// @route  GET api/profile
+// @desc   GET current user profiles
+//@access  Private
+router.get('/', passport.authenticate('jwt', { session: false}), (req, res) => {
+    
+    const errors = {};
+    
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'No profile found for this user';
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status.json(err))
+    });
 
 module.exports = router;
