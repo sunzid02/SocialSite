3cde93e755a7990f8c413635f5c860de76b882c0
Sarker Sunzid Mahmud <sunzid02@gmail.com>
2025-05-07 15:26:59 +0200

    - Post Like & Unlike Routes

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -96,4 +96,74 @@ router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, re
 });
 
 
+// @route  POST api/posts/like/:postId
+// @desc   Like post
+//@access  Private
+router.post('/like/:postId', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+
+            Post.findById(req.params.postId)
+                .then(post => {
+                    
+                    //already liked the post
+                    if (post.likes.filter(like => like.user.toString() === req.user.id).lenth > 0) {
+                        return res.status(400).json({ alreadyLike: 'User already like this post...'})                   
+                    }
+
+                    //Add user id to likes array
+                    post.likes.unshift({ user: req.user.id });
+
+                    post.save().then(post => res.json(post))
+                })
+                .catch(err => {
+                    console.error("Error finding post:", err);
+                    res.status(404).json({ postnotfound: 'No post found' });
+                });
+        })
+        .catch(err => {
+            console.error("Error finding profile:", err);
+            res.status(500).json({ error: 'Server error' });
+        });
+});
+
+
+// @route  POST api/posts/unlike/:postId
+// @desc   Remove like from the post
+//@access  Private
+router.post('/unlike/:postId', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+
+            Post.findById(req.params.postId)
+                .then(post => {
+                    
+                    //already liked the post
+                    if (post.likes.filter(like => like.user.toString() === req.user.id).lenth === 0) {
+                        return res.status(400).json({ notliked: 'You have not liked this post yet...'})                   
+                    }
+
+                    //Get Remove index
+                    const removeIndex = post.likes
+                        .map(item => item.user.toString())
+                        .indexOf(req.user.id);
+
+                    //Splice out of array
+                    post.likes.splice(removeIndex, 1);
+
+                    //save
+                    post.save().then( post => res.json(post));
+                })
+                .catch(err => {
+                    console.error("Error finding post:", err);
+                    res.status(404).json({ postnotfound: 'No post found' });
+                });
+        })
+        .catch(err => {
+            console.error("Error finding profile:", err);
+            res.status(500).json({ error: 'Server error' });
+        });
+});
+
+
 module.exports = router;
