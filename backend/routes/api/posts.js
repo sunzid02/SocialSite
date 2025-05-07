1a8ef23f06dadd237767c780860e0ca38c9b343b
Sarker Sunzid Mahmud <sunzid02@gmail.com>
2025-05-07 16:52:38 +0200

    - Add & Remove Comments

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -166,4 +166,74 @@ router.post('/unlike/:postId', passport.authenticate('jwt', { session: false }),
 });
 
 
+// @route  POST api/posts/comment/:postId
+// @desc   Add a comment to the post
+//@access  Private
+router.post('/comment/:postId', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+    Post.findById(req.params.postId)
+        .then(post => {
+
+            //validation    
+            const {errors, isValid } = validatePostInput(req.body);
+
+            //check validation
+            if (!isValid) 
+            {
+                //return errors with 400
+                return res.status(400).json(errors);    
+            }
+             
+            //create comment
+            const newComment = {
+                text: req.body.text,
+                name: req.body.name,
+                avatar: req.body.avatar,
+                user: req.user.id
+            }
+
+            // Add to comments array
+            post.comments.unshift(newComment);
+            
+            //save
+            post.save()
+                .then(post => res.json(post))
+                .catch(err => res.json({ comment: 'comment insertion failed'}))
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'post not found' + err }))
+});
+
+// @route  DELETE api/posts/comment/:postId/:commentId
+// @desc   Remove a comment from the post
+//@access  Private
+router.delete('/comment/:postId/:commentId', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+    Post.findById(req.params.postId)
+        .then(post => {
+
+            //Check to see if the comment exists
+            if(post.comments.filter(comment => comment._id.toString() === req.params.commentId).length === 0 )
+            {
+                return res.status(404).json({ commentnotexist: 'comment does not exist'});
+            }
+
+            //Get remove index
+            const removeIndex = post.comments
+                        .map(item => item._id.toString())
+                        .indexOf(req.params.commentId)
+
+            //Splice comment out of array
+            post.comments.splice(removeIndex, 1);
+
+            post.save()
+                .then(post => res.json(post))
+                .catch(err => res.status(500).json({
+                    ailed: 'comment deletion failed'
+                })) 
+
+
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'post not found' + err }))
+});
+
 module.exports = router;
--- backend/validation/post.js
+++ backend/validation/post.js
@@ -8,16 +8,14 @@ module.exports = function validatePostInput(data){
 
     data.text = !isEmpty(data.text) ? data.text : '';
 
-    //post
-    if(!Validator.isLength(data.text, {min: 10, max: 300})){
-         errors.text = 'Post must be between 10 and 300 characters';
-    }
-
-    if (Validator.isEmpty(data.text)) 
-    {
+    // Check if text is empty
+    if (Validator.isEmpty(data.text)) {
         errors.text = 'Post text is required';
-    }    
-
+    } 
+    else if (!Validator.isLength(data.text, { min: 10, max: 300 })) 
+    {
+        errors.text = 'Post must be between 10 and 300 characters';
+    }
 
     return {
         errors,
