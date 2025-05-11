6c01e42e91130a9e684961154b8b38f78070bb46
Sarker Sunzid Mahmud <sunzid02@gmail.com>
2025-05-11 10:41:46 +0200

    - Creating the Login Form with State

--- backend/my-app/src/components/auth/Login.js
+++ backend/my-app/src/components/auth/Login.js
@@ -1,10 +1,76 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      email:'',
+      password:'',
+      errors:{}
+    };
+
+    // binding
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e){
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    }
+
+    console.log(user);
+    
+  }
+
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <section className="container">
+            {/* <!-- Alert --> */}
+            {/* <div className="alert alert-danger">
+              Invalid Credentials
+            </div> */}
+
+            <h1 className="large text-primary">
+              Sign In
+            </h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign into your account</p>
+
+            <form  className="form" onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <input 
+                  type="email" 
+                  name="email" 
+                  value={this.state.email}
+                  placeholder="Email Address" 
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <input 
+                  type="password" 
+                  name="password" 
+                  placeholder="Password" 
+                  value={this.state.password}
+                  onChange={this.onChange}  
+                />
+              </div>
+              <input type="submit" value="Login" className="btn btn-primary" />
+            </form>
+
+            <p className="my-1">
+              Don't have an account? <a href="register.html">Sign Up</a>
+            </p>
+        </section>
       </div>
     )
   }
